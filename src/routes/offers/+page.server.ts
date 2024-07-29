3729a205220af1b6ea12ef3738d083891b0dc7ed
Blaine Cook <romeda@gmail.com>
2024-07-29 12:19:33 -0700

    fix api key

--- src/routes/offers/+page.server.ts
+++ src/routes/offers/+page.server.ts
@@ -1,8 +1,9 @@
 import { GristDocAPI } from 'grist-api';
+import { GRIST_API_KEY } from '$env/static/private';
 
 export async function load() {
 	const DOC_URL = 'https://slocan.getgrist.com/wrs8LmMCRoWxA6PzLuyWj4';
-	const api = new GristDocAPI(DOC_URL, { apiKey: '***REMOVED***' });
+	const api = new GristDocAPI(DOC_URL, { apiKey: GRIST_API_KEY });
 	const data = await api.fetchTable('Offers');
 	console.log(data);
 	return { data };
