88fd2969c177daacdb22608f98b4b29cbba57f08
Boris Anthony <964649+BorisAnthony@users.noreply.github.com>
2024-07-31 15:10:07 +0200

    Overhaul…

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,51 @@
-<h2 class="h2">
+<!-- <h2 class="h2">
 	Shared Resources for Communities affected by the Slocan Lake & Kootenay Lake Wildfires
-</h2>
+</h2> -->
+<div class="h3 my-10">
+	Shared Community Resources 
+	<div class="h1">Slocan & Kootenay Lake Wildfires</div>
+</div>
+
+<!-- <p>
+	If you've been evacuated, are under alert, or are looking for information about the wildfires,
+	please start by accessing the 
+	<a class="btn variant-ghost-primary" href="/resources">Provincial and District Resources</a> 
+	as they are the most up-to-date and comprehensive sources of information.
+</p> -->
+
+<a class="block w-full btn variant-ghost-primary text-xl font-bold py-3 mb-1" href="/resources">Latest<br>Information & Dashboards</a> 
+<p class="text-center italic">access Provincial & District sources</p>
+
+<hr class="!border-t-1 my-6" />
+
+<div class="flex justify-around gap-4">
+
+	<a class="card variant-ghost-warning card-hover w-full text-center sm:w-1/2" href="/offers">
+		<div class="w-full pt-4">
+				<svg class="mx-auto fill-warning-600 w-[8rem] h-[8rem]" viewBox="0 0 320 256"><path d="M179 9c23-4 48 35 35 44-14 8-9-18-23-25l-18-6c-3-7 1-11 6-13Z"/><path d="m145 22 92 91c7-3-3-35 7-52 8-11 28-8 32 5 3 10 2 101-1 115a94 94 0 0 1-134 56c-9-4-84-79-90-85-11-11 7-27 17-20l45 41c8 6 16-3 9-10l-64-63c-9-10 10-30 22-18l68 62c6 7 16-4 8-10L81 62c-9-12 11-31 23-19l70 70c5 4 15-3 9-10l-64-63c-12-11 11-34 26-18ZM42 176c10-1 6 20 20 28 4 1 19 1 18 10 0 12-22 5-28 2-12-9-25-38-10-40Z"/></svg>
+		</div>
+		<div class="p-4 mb-2 mx-auto md:text-2xl sm:text-xl text-base font-bold text-warning-700" data-toc-ignore>Looking <span class="whitespace-nowrap">for help</span></div>
+	</a>
+
+	<a class="card variant-ghost-success card-hover w-full text-center sm:w-1/2" href="https://slocan.getgrist.com/forms/qhosjjXEfVpTGCTrVacWne/25">
+		<div class="w-full pt-4">
+				<svg class="mx-auto fill-success-700 w-[8rem] h-[8rem]" viewBox="0 0 320 256"><path d="M161 6c8 0 15 7 15 15v99c0 5 3 8 7 8 5 0 8-3 8-8V37a15 15 0 1 1 31 0v83c0 5 3 8 7 8s8-3 8-8V67a15 15 0 1 1 30 0v93c-5 2-10 7-12 13l-4 15-15 4a20 20 0 0 0 0 39 83 83 0 0 1-53 19h-9c-28 0-55-11-76-30l-54-51a19 19 0 0 1 27-28l29 27V37a15 15 0 1 1 30 0v83c0 5 3 8 8 8 4 0 7-3 7-8V21c0-8 7-15 16-15Zm-4 156c-1-2-2-3-4-3s-4 1-4 3l-4 12-12 3c-2 1-3 2-3 4s1 4 3 5l12 3 4 12c0 2 2 3 4 3s3-1 4-3l4-12 12-3c1-1 3-3 3-5s-2-3-3-4l-12-3-4-12ZM58 63l23 7a5 5 0 0 1 0 9l-23 7-7 23a5 5 0 0 1-10 0l-6-23-23-7a5 5 0 0 1 0-9l23-7 6-23a5 5 0 0 1 10 0l7 23Zm228 137 23 7a5 5 0 0 1 0 10l-23 6-6 23a5 5 0 0 1-10 0l-7-23-23-6a5 5 0 0 1 0-10l23-7 7-23a5 5 0 0 1 10 0l6 23Z"/></svg>
+		</div>
+		<div class="p-4 mb-2 mx-auto md:text-2xl sm:text-xl text-base font-bold text-success-700" data-toc-ignore>Offering help</div>
+	</a>
+
+</div>
+
+
+<hr class="!border-t-1 my-8" />
 
 <p>
 	This is a community-led site to care for our friends and neighbours during these difficult
-	times. We're just starting to build this resource, so please bear with us and please <a
-		class="btn-sm variant-ghost-secondary"
-		href="/contribute">contribute</a
-	> if you're able.
-</p>
-
+	times.
+	</p>
 <p>
-	If you've been evacuated, are under alert, or are looking for information about the wildfires,
-	please start by accessing the <a class="btn variant-ghost-primary" href="/resources"
-		>Provincial and District Resources</a
-	> as they are the most up-to-date and comprehensive sources of information.
-</p>
-
-<p>
-	If you're an evacuee looking for help, you can <a
-		href="/offers"
-		class="btn variant-ghost-primary">View Offers of Help</a
-	>.
-</p>
-
-<p>
-	If you have something to offer evacuees, you can <a
-		href="https://slocan.getgrist.com/forms/qhosjjXEfVpTGCTrVacWne/25"
-		class="btn variant-ghost-primary">Offer Your Help</a
-	>.
+	We're just starting to build this resource, so please bear with us and please 
+	<a class="btn-sm variant-ghost-secondary" href="/contribute">contribute</a> if you're able.
 </p>
 
 <p>
--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,48 +1,39 @@
-<h2 class="h2">Provincial Resources</h2>
+<hr class="!border-t-0 my-4" />
 
+<!-- <h2 class="h2">Provincial Resources</h2> -->
 <ul>
 	<li>
-		<a href="https://ess.gov.bc.ca/" class="anchor">
-			Provincial Emergency Support Services</a
-		> Register here for support if you've been evacuated.
-	</li>
-	<li>
-		<a
-			class="anchor"
-			href="https://wildfiresituation.nrs.gov.bc.ca/incidents?fireYear=2024&incidentNumber=N51065&source=map"
-			>Aylwin Creek Wildfire Dashboard</a
-		>
-	</li>
-	<li>
-		<a
-			class="anchor"
-			href="https://wildfiresituation.nrs.gov.bc.ca/incidents?fireYear=2024&incidentNumber=N51210&source=map"
-			>Komonko Creek Wildfire Dashboard</a
-		>
-	</li>
-	<li>
-		<a
-			class="anchor"
-			href="https://wildfiresituation.nrs.gov.bc.ca/incidents?fireYear=2024&incidentNumber=N71058&source=map"
-			>Argenta Wildfire Dashboard</a
-		>
-	</li>
-	<li>
-		<a
-			class="anchor"
-			href="https://wildfiresituation.nrs.gov.bc.ca/incidents?fireYear=2024&incidentNumber=N51069&source=map"
-			>Ponderosa Wildfire Dashboard</a
-		>
-	</li>
-	<li>
-		<a
-			class="anchor"
-			href="https://wildfiresituation.nrs.gov.bc.ca/incidents?fireYear=2024&incidentNumber=N51117&source=map"
-			>Mulvey Creek Wildfire Dashboard</a
-		>
+		<a href="https://ess.gov.bc.ca/" class="anchor font-semibold text-lg">Provincial Emergency Support Services</a><br>
+			Register here if you've been evacuated.
 	</li>
 </ul>
 
-<h2 class="h2">RDCK Resources</h2>
 
-<a href="https://rdck.ca/eocinfo" class="anchor">RDCK Emergency Operations Center</a>
+<hr class="!border-t-1 my-4" />
+
+
+<!-- <h2 class="h2">RDCK Resources</h2> -->
+<a href="https://rdck.ca/eocinfo" class="anchor font-semibold text-lg">RDCK Emergency Operations Center</a>
+
+<hr class="!border-t-1 my-4" />
+
+
+<h2 class="h2">Widlfire Dashboards</h2>
+<ul class="flex justify-around flex-wrap">
+	<li class="dashboard-list">
+		<a href="https://wildfiresituation.nrs.gov.bc.ca/incidents?fireYear=2024&incidentNumber=N51065&source=map">Aylwin Creek</a>
+	</li>
+	<li class="dashboard-list">
+		<a href="https://wildfiresituation.nrs.gov.bc.ca/incidents?fireYear=2024&incidentNumber=N51210&source=map">Komonko Creek</a>
+	</li>
+	<li class="dashboard-list">
+		<a href="https://wildfiresituation.nrs.gov.bc.ca/incidents?fireYear=2024&incidentNumber=N71058&source=map">Argenta</a>
+	</li>
+	<li class="dashboard-list">
+		<a href="https://wildfiresituation.nrs.gov.bc.ca/incidents?fireYear=2024&incidentNumber=N51069&source=map">Ponderosa</a>
+	</li>
+	<li class="dashboard-list">
+		<a href="https://wildfiresituation.nrs.gov.bc.ca/incidents?fireYear=2024&incidentNumber=N51117&source=map">Mulvey Creek</a>
+	</li>
+</ul>
+
